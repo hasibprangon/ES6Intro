25b4f9ad83da826a119b19831257ccb61beae44c
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-09 15:35:34 +0600

    break

---
+++ ES6Intro/js/dom.js
@@ -0,0 +1,7 @@
+document.getElementById('apply-bg').addEventListener('click', function (){
+   const friends = document.getElementsByClassName("friend");
+//    console.log(friends)
+for(const friend of friends){
+    console.log(friend);
+}
+});
