cc3fab1088e4326bb4fe4065c72dc7860794fbe8
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-11 21:23:47 +0600

    ignoring default value

--- ES6Intro/js/default.js
+++ ES6Intro/js/default.js
@@ -1,12 +1,17 @@
-function add(first, second) {
-    console.log(first, second);
-    second = second || 0;
+// function add(first, second) {
+//     console.log(first, second);
+//     // second = second || 0;
 
-    // if(second===undefined ){
-    //     second = 0
-    // }
+//     // if(second===undefined ){
+//     //     second = 0
+//     // }
+//     const total = first + second;
+//     return total;
+// }
+
+function add (first ,second = 0){
     const total = first + second;
     return total;
 }
-const result = add(10);
+const result = add(10, 5);
 console.log(result)
