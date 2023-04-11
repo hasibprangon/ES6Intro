813671d06f291ef5bc960856ed585acd6fc8f664
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-11 21:48:15 +0600

    template

---
+++ ES6Intro/js/template.js
@@ -0,0 +1,7 @@
+const a = 10;
+const b = 20;
+const nums = [1, 2, 3, 4, 5, 6];
+const sum = `sum of ${a} and ${b} is : ${a+b}`;
+console.log(sum);
+const lenght = `lenght of nums is: ${nums.length} `;
+console.log(lenght);
