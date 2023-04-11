f7b91c0611a5529b1b00a71f6216fc7dbe14255c
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-11 21:18:17 +0600

    shortcut condition

--- ES6Intro/js/default.js
+++ ES6Intro/js/default.js
@@ -1,10 +1,12 @@
-function add (first , second){
-    console.log(first , second);
-    if(second===undefined ){
-        second = 0
-    }
+function add(first, second) {
+    console.log(first, second);
+    second = second || 0;
+
+    // if(second===undefined ){
+    //     second = 0
+    // }
     const total = first + second;
     return total;
 }
-const result = add (10);
+const result = add(10);
 console.log(result)
